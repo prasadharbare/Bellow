5f48d90711308e8d60c467da4308cd6f2ddc4acc
gliese-sketch <173663566+gliese-sketch@users.noreply.github.com>
2024-11-11 07:15:58 +0000

    explore details summary

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 function App() {
-  return <div className="text-xl">App</div>;
+  return (
+    <div className="text-xl">
+      <details>
+        <summary>Click me</summary>
+        <p>Bellow</p>
+      </details>
+    </div>
+  );
 }
 
 export default App;
