e55ffb0c59dcccd03f7f8d63b0d2d01097abdeb7
prasadharbare <prasad1523@proton.me>
2024-11-21 07:54:21 +0000

    yes

--- src/Accordion.jsx
+++ src/Accordion.jsx
@@ -1,19 +1,55 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 function Accordion({
   title = "Title",
   description = "Description",
   isOpen = false,
   underline = true,
+  showWordCount = false,
+  onClick = () => {},
 }) {
+  const [count, setCount] = useState(0);
+  
+  useEffect(() => {
+    fetch("https://wakati.prasad15.workers.dev/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        text: description,
+      }),
+    })
+      .then((res) => res.json())
+      .then((data) => setCount(data.seconds));
+    console.log("API called");
+    
+  },[]);
+
+  // fetch("https://wakati.prasad15.workers.dev/", {
+  //   method: "POST",
+  //   headers: {
+  //     "Content-Type":"application/json",
+  //   },
+  //   body: JSON.stringify({
+  //     text:description,
+  //   })
+  // })
+  //   .then((res) => res.json())
+  // .then((data)=>setCount(data.wordCount));
+
   return (
-    <details className="w-80 bg-stone-100 m-1 p-2 rounded" open={isOpen}>
-      <summary
-        className={`cursor-pointer font-bold text-stone-900 ${
-          underline && "hover:underline"
-        }`}
-      >
-        {title}
+    <details
+      className="w-80 bg-stone-100 m-1 p-2 rounded"
+      open={isOpen}
+      onClick={() => {onClick(title)
+      }}
+    >
+      <summary className={`cursor-pointer font-bold text-stone-900`}>
+        <span className={`${underline && "hover:underline"}`}>{title}</span>
+        <span className="font-normal text-zinc-600 text-sm ml-3">
+          {`${showWordCount}` && `${count} words`}
+        </span>
       </summary>
       <p className="text-stone-800">{description}</p>
     </details>
--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,18 @@
 import Bellow from "./Bellow";
+import items from "./assets/items.json";
 
 function App() {
-  const debates = [
-    {
-      title: "batman",
-      description: "Marvels are awesome",
-    },
-    {
-      title: "Coke TWO Pepsi",
-      description: "Coke is better",
-    },
-    {
-      title: "React vs Angular",
-      description: "React is better",
-    },
-    {
-      title: "Java two Kotlin",
-      description: "Kotlin is better",
-    },
-    {
-      title: "Coke vs Pepsi",
-      description: "Kotlin is better. This is changed!",
-    },
-  ];
-
+  const [lastClicked]
   return (
-    debates && (
-      <div className="flex justify-center items-center h-screen">
-        <Bellow data={debates} filterKeyword="" isOpen underline={false} />
-      </div>
-    )
+    <div className="flex justify-center items-center h-screen">
+      <Bellow
+        data={items}
+        filterKeyword=""
+        isOpen
+        underline={false}
+        showWordCount
+      />
+    </div>
   );
 }
 
